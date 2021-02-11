6dc735d8c045540e040f211b8b805a06b72a1504
YoWiDiN <yowidin@gmail.com>
2021-02-11 10:58:56 +0100

    Updated CI scripts

--- .ci/build-android.py
+++ .ci/build-android.py
@@ -2,8 +2,8 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add(settings={"arch": "armv7"})
-    builder.add(settings={"arch": "armv8"})
-    builder.add(settings={"arch": "x86"})
-    builder.add(settings={"arch": "x86_64"})
+    builder.add(settings={"arch": "armv7", "os.api_level": "16"})
+    builder.add(settings={"arch": "armv8", "os.api_level": "21"})
+    builder.add(settings={"arch": "x86", "os.api_level": "16"})
+    builder.add(settings={"arch": "x86_64", "os.api_level": "21"})
     builder.run()
