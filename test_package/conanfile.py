6959685a1beadb0595f162e317462b588a544b80
YoWiDiN <yowidin@gmail.com>
2021-02-11 10:01:54 +0100

    Apple M1 tests

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -18,5 +18,5 @@ class Test(ConanFile):
 
     def test(self):
         assert os.path.exists(os.path.join(self.deps_cpp_info["zlib"].rootpath, "licenses", "LICENSE"))
-        if "x86" in self.settings.arch and not tools.cross_building(self.settings):
+        if not tools.cross_building(self.settings):
             self.run(os.path.join("bin", "test"), run_environment=True)
