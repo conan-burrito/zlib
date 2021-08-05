73abbc86b2e0633e6c518207d6df85932542f073
YoWiDiN <yowidin@gmail.com>
2021-08-05 19:44:02 +0200

    Fixed watchOS arch

--- .ci/build-ios.py
+++ .ci/build-ios.py
@@ -3,6 +3,6 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add(settings={"os": "iOS", "os.version": "9.0"})
-    builder.add(settings={"os": "watchOS", "os.version": "4.0", "arch": "armv8"})
+    builder.add(settings={"os": "watchOS", "os.version": "4.0", "arch": "armv7k"})
     builder.add(settings={"os": "tvOS", "os.version": "11.0", "arch": "armv8"})
     builder.run()
